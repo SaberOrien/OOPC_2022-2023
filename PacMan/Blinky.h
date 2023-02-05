f5212ed15cb3f61e122e50b2075ac761ae67fe85
SaberOrien <115357207+SaberOrien@users.noreply.github.com>
2023-02-05 20:59:40 +0100

    Delete PacMan directory

--- PacMan/Blinky.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Ghost.h"
-
-class Blinky : public Ghost{
-    Q_OBJECT
-  public:
-    Blinky();
-};
--- PacMan/Wall.cpp
+++
@@ -1,8 +0,0 @@
-#include "Wall.h"
-#include "Constants.h"
-#include <QBrush>
-
-Wall::Wall(){
-    setRect(0, 0, BLOCK_SIZE, BLOCK_SIZE);
-    setBrush(QBrush(Qt::cyan, Qt::SolidPattern));
-}
