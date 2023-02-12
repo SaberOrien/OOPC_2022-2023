c7730bb91fe8dde9b05ac40598efe964d093ed49
SaberOrien <115357207+SaberOrien@users.noreply.github.com>
2023-02-12 16:52:14 +0100

    Delete PacMan directory

--- PacMan/EndScreen.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <QGraphicsTextItem>
-#include <string>
-
-class Ending : public QGraphicsTextItem {
-  public:
-    Ending(std::string msg, int option, QGraphicsItem* parent=0);
-};
--- PacMan/Wall.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <QGraphicsRectItem>
-
-class Wall : public QGraphicsRectItem {
-  public:
-    Wall(int mode);
-};
